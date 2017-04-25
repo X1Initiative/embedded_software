fb9a379bc279b79fb3876e4eabe1b4c35dcc9008
ASMEBruins <ASME.Bruins@gmail.com>
2017-04-25 04:11:47 +0000

    No changes.

--- Motor_spin.c
+++ Motor_spin.c
@@ -1,12 +1,15 @@
 
 #include <stdio.h>
 #include <wiringPi.h>
+#include <time.h>
+
 
 int PIN = 7; 
 
 void setup()
 {
-	wiringPiSetup ();
+	
+	wiringPiSetup (); 
 	int PULSE = 1000; 
 	pinMode (PIN,OUTPUT);
 	int arming_time; 
@@ -22,19 +25,15 @@ void setup()
 void sample()
 {
 	int PULSE = 1000; 
-	for (PULSE = 1000; PULSE <= 1900; PULSE++)
+	for (PULSE = 1000; PULSE <= 2000; PULSE+=10)
 	{
 		digitalWrite(PIN,HIGH); 
 		delayMicroseconds(PULSE); 
 		digitalWrite(PIN,LOW); 
 		delay(20 - (PULSE/1000));
-		if (PULSE == 1250 || PULSE == 1500 || PULSE == 1750)
-		{
-			printf("STEP\n");
-		}	
 	}
 	
-	for (PULSE = PULSE; PULSE >= 1000; PULSE -= 100)
+	for (PULSE = PULSE; PULSE >= 1000; PULSE -= 10)
 	{
 	
 		digitalWrite(PIN,HIGH); 
