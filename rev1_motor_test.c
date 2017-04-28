8ab3d370c9f158011bd0b0baee36ac233089163a
PrescottRynewicz <pjrynewicz@gmail.com>
2017-04-27 22:27:29 -0700

    Updated first version of rev! testing code. Gives ability to simeautanesouly control robot and send new input signals.

---
+++ rev1_motor_test.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+//#include <wiringPi.h>
+#include <time.h>
+#include <pthread.h>
+
+
+
+int global_PULSE = 1000;
+pthread_mutex_t lock;
+// void setup()
+// {
+//
+// 	wiringPiSetup ();
+// 	int PULSE = 1000;
+// 	pinMode (PIN,OUTPUT);
+// 	int arming_time;
+// 	for (arming_time = 0; arming_time < 200; arming_time++)
+// 	{
+// 		digitalWrite(PIN,HIGH);
+// 		delayMicroseconds(PULSE);
+// 		digitalWrite(PIN,LOW);
+// 		delay(20 - (PULSE/1000));
+// 	}
+// }
+
+void input_wait(void *ptr)
+{
+    int *current_val;
+    current_val = (int *) ptr;
+    while (1)
+    {
+        pthread_mutex_lock(&lock);
+        printf("Enter New Value:");
+        scanf("%d",current_val);
+        pthread_mutex_unlock(&lock);
+    }
+}
+
+// Main takes initial input for
+int main()
+{
+    // setup();
+
+    pthread_mutex_init (&lock,NULL);
+    printf("Enter Starting Motor Pulse Speed in MicroSecond: ");
+    scanf("%d",&global_PULSE);
+    pthread_t thread;
+    pthread_create(&thread, NULL, (void *) &input_wait, (void *) &global_PULSE);
+
+    int test = global_PULSE;
+    while (1)
+    {
+        //This code here will run on an infinite loop until the program stops
+        // The Threaded function will continue to look for new inputs
+        // test code below.
+
+        if (test != global_PULSE)
+        {
+            test = global_PULSE;
+            pthread_mutex_lock(&lock);
+            printf("New Pulse being run: %d\n",global_PULSE);
+            pthread_mutex_unlock(&lock);
+        }
+
+        /*
+        for (global_PULSE = 1000; global_PULSE <= 2000; global_PULSE+=10)
+    	{
+    		digitalWrite(PIN,HIGH);
+    		delayMicroseconds(global_PULSE);
+    		digitalWrite(PIN,LOW);
+    		delay(20 - (global_PULSE/1000));
+    	}
+
+    	for (global_PULSE = global_PULSE; global_PULSE >= 1000; global_PULSE -= 10)
+    	{
+
+    		digitalWrite(PIN,HIGH);
+    		delayMicroseconds(global_PULSE);
+    		digitalWrite(PIN,LOW);
+    		delay(20 - (global_PULSE/1000));
+    	}
+        */
+    }
+
+    pthread_join(&thread,NULL);
+
+    printf("\nCurrent: %d\n", global_PULSE);
+
+    return (0);
+
+}
