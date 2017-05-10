4fffe750d170f244c88d4afb08612f32d6d132e8
ASMEBruins <ASME.Bruins@gmail.com>
2017-05-10 04:37:31 +0000

    Updated motor code to take inputs for all four motors. Commented code for algorithm to send correct output signals without added threading.

--- rev1_motor_test.c
+++ rev1_motor_test.c
@@ -44,6 +44,88 @@ void setup()
 		delay(20 - (PULSE/1000));
 	}
 }
+/* Helper function for finding the max of two numbers */
+int max(int x, int y)
+{
+    if(x > y)
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
+}
+
+/* left is the index of the leftmost element of the subarray; right is one
+ * past the index of the rightmost element */
+void merge_helper(int *input, int left, int right, int *scratch)
+{
+    /* base case: one element */
+    if(right == left + 1)
+    {
+        return;
+    }
+    else
+    {
+        int i = 0;
+        int length = right - left;
+        int midpoint_distance = length/2;
+        /* l and r are to the positions in the left and right subarrays */
+        int l = left, r = left + midpoint_distance;
+
+        /* sort each subarray */
+        merge_helper(input, left, left + midpoint_distance, scratch);
+        merge_helper(input, left + midpoint_distance, right, scratch);
+
+        /* merge the arrays together using scratch for temporary storage */ 
+        for(i = 0; i < length; i++)
+        {
+            /* Check to see if any elements remain in the left array; if so,
+             * we check if there are any elements left in the right array; if
+             * so, we compare them.  Otherwise, we know that the merge must
+             * use take the element from the left array */
+            if(l < left + midpoint_distance && 
+                    (r == right || max(input[l], input[r]) == input[l]))
+            {
+                scratch[i] = input[l];
+                l++;
+            }
+            else
+            {
+                scratch[i] = input[r];
+                r++;
+            }
+        }
+        /* Copy the sorted subarray back to the input */
+        for(i = left; i < right; i++)
+        {
+            input[i] = scratch[i - left];
+        }
+    }
+}
+
+/* mergesort returns true on success.  Note that in C++, you could also
+ * replace malloc with new and if memory allocation fails, an exception will
+ * be thrown.  If we don't allocate a scratch array here, what happens? 
+ *
+ * Elements are sorted in reverse order -- greatest to least */
+
+int mergesort(int *input, int size)
+{
+    int *scratch = (int *)malloc(size * sizeof(int));
+    if(scratch != NULL)
+    {
+        merge_helper(input, 0, size, scratch);
+        free(scratch);
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+	
 
 void input_wait(void *ptr)
 {
@@ -57,12 +139,17 @@ void input_wait(void *ptr)
         // Input value in format 1000 1000 1000 1000
 		scanf("%d %d %d %d", &current_val[0], &current_val[1], &current_val[2], &current_val[3]);
         pthread_mutex_unlock(&lock);
+        
+        // perform calcs here to set output values for the order of which motors will be turned off
+        // along with their speeds. 
     }
 }
 
 // Main takes initial input for
 int main()
 {
+	
+
     setup();
 	
     pthread_mutex_init (&lock,NULL);
@@ -87,34 +174,31 @@ int main()
         // The Threaded function will continue to look for new inputs
         // test code below.
         
-        /* Test code: Not for use. 
-        if (test != global_PULSE)
-        {
-            test = global_PULSE;
-            pthread_mutex_lock(&lock);
-            printf("New Pulse being run: %d\n",global_PULSE);
-            pthread_mutex_unlock(&lock);
-        }*/
+        //Adjust code here to deal with new algorithm of starting with the greatest
+        // pulse, and waiting after each one so that all are sinked up. 
         
-			digitalWrite(PIN[0],HIGH);
-			digitalWrite(PIN[1],HIGH);
-			digitalWrite(PIN[2],HIGH);
-			digitalWrite(PIN[3],HIGH);
-
-			delayMicroseconds(global_PULSE[0]);
-			delayMicroseconds(global_PULSE[1]);
-			delayMicroseconds(global_PULSE[2]);
-			delayMicroseconds(global_PULSE[3]);
-			
-			digitalWrite(PIN[0],LOW);
-			digitalWrite(PIN[1],LOW);
-			digitalWrite(PIN[2],LOW);
-			digitalWrite(PIN[3],LOW);
-			
-			delay(20 - (global_PULSE[0]/1000));
-			delay(20 - (global_PULSE[1]/1000));
-			delay(20 - (global_PULSE[2]/1000));
-			delay(20 - (global_PULSE[3]/1000));
+        // all motors start at same time. 
+        // motors turn off from low throttle -> high throttle. 
+        
+		digitalWrite(PIN[0],HIGH);
+		digitalWrite(PIN[1],HIGH);
+		digitalWrite(PIN[2],HIGH);
+		digitalWrite(PIN[3],HIGH);
+		
+		delayMicroseconds(global_PULSE[0]);
+		delayMicroseconds(global_PULSE[1]);
+		delayMicroseconds(global_PULSE[2]);
+		delayMicroseconds(global_PULSE[3]);
+		
+		digitalWrite(PIN[0],LOW);
+		digitalWrite(PIN[1],LOW);
+		digitalWrite(PIN[2],LOW);
+		digitalWrite(PIN[3],LOW);
+		
+		delay(20 - (global_PULSE[0]/1000));
+		delay(20 - (global_PULSE[1]/1000));
+		delay(20 - (global_PULSE[2]/1000));
+		delay(20 - (global_PULSE[3]/1000));
     }
 
     pthread_join(thread,NULL);
